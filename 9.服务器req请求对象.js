e8a2d1964b351fb966afd0a395a5ed8cf54196c6
gl <763419081@qq.com>
2022-04-05 23:23:12 +0800

    修改了 服务器req请求对象.js 文件

--- 9.服务器req请求对象.js
+++ 9.服务器req请求对象.js
@@ -2,10 +2,14 @@ const http = require('http');
 
 const server = http.createServer();
 
-server.on('request',function(req,res){
+server.on('request',(req,res)=>{
+    //req.url 是客户端请求的url地址
+    //req.method 是客户端请求时的method类型  GET  POST
     var str = `请求地址：${req.url} 
     请求类型为：${req.method}`;
     console.log(str) ;
+    //调用res.end()方法 ，向客户端相应一些内容
+    res.end(str)
 })
 
 server.listen(80,()=>{
